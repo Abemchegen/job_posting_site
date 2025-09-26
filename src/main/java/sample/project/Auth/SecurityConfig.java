c0a640a9970591c2a2d0be420b36c76fce535afd
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2025-09-26 18:45:02 +0300

    cors fix

--- src/main/java/sample/project/Auth/SecurityConfig.java
+++ src/main/java/sample/project/Auth/SecurityConfig.java
@@ -48,8 +48,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("sira-git-pre-keycloak-tempnia945-gmailcoms-projects.vercel.app/"));
-        configuration.setAllowedMethods(List.of("*"));
+        configuration
+                .setAllowedOrigins(List.of("https://sira-git-pre-keycloak-tempnia945-gmailcoms-projects.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
